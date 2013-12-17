2b1dde5f3e20ad54495688c1ef1a9094aafd0486
Andrei <andrei@m$win>
2013-12-18 00:01:09 +0200

    finally running something

--- src/VBAM/Direct_Access_Image_Sample.cpp
+++ src/VBAM/Direct_Access_Image_Sample.cpp
@@ -39,17 +39,20 @@ int _tmain(int argc, _TCHAR* argv[])
 	KImage *outputs[50];
 	KImage *confidences[50];
 	int j = 0;
-	for (int i = 0; i < argc; i+= 2) {
+	for (int i = 1; i < argc; i+= 2) {
 
-		_tprintf(_T("%s %s\n", argv[i], argv[i+1]));
+		TCHAR strNewFileName[0x100];
+         _tprintf(_T("%s %s\n"), argv[i], argv[i + 1]);
 		KImage *pImage = new KImage(argv[i]);
 
-		if (pImage != NULL) {
+		if (pImage != NULL && pImage->IsValid()) {
 			outputs[j] = pImage;
+			TCHAR strNewFileName[0x100];
 			confidences[j] = new KImage(argv[i + 1]);
 			j++;
 		} else {
 			std::cout << "Wrong input image\n";
+			return -100;
 		}
 	}
 
